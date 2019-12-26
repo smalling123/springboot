787fe7288bbad06f73328a264fd5f5917035ef2e
smalling <1101938628@qq.com>
2019-12-26 18:10:18 +0800

    Initial commitgg

---
+++ demo/src/test/java/com/mamway/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
